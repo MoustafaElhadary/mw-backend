0a1947078c2b2d03c2f16f5d310ebe6d4b9ade2c
moustafaElhadary <moustafaElhadary96@gmail.com>
2021-11-20 01:33:24 -0500

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,11 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import {
-    ApolloClient,
-    ApolloProvider,
-    createHttpLink,
-    InMemoryCache
-} from '@apollo/client';
 import axios from 'axios';
-import fetch from 'isomorphic-fetch';
 import { NextComponentType, NextPageContext } from 'next';
 import { AppProps } from 'next/app';
-import React, { ReactElement, useEffect } from 'react';
-import { Provider, useDispatch } from 'react-redux';
-import { setSession } from 'redux/authSlice';
-import store, { useAppSelector } from 'redux/store';
-import { setProfile } from 'redux/userSlice';
+import React, { ReactElement } from 'react';
+import { Provider } from 'react-redux';
+import store from 'redux/store';
 import { SWRConfig } from 'swr';
-import { definitions } from 'types/supabase';
-import { supabase } from 'utils/supabaseClient';
-import '../styles/index.css';
-
-const apolloClient = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: createHttpLink({
-    uri: `https://${process.env.NEXT_PUBLIC_STORE}.myshopify.com/api/2021-07/graphql.json`,
-    headers: {
-      'X-Shopify-Storefront-Access-Token': process.env.NEXT_PUBLIC_TOKEN,
-      Accept: 'application/graphql',
-    },
-    fetch,
-  }),
-});
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
   return (
@@ -46,53 +22,15 @@ function AppWrapper({
   Component: NextComponentType<NextPageContext, any, {}>;
   pageProps: any;
 }): ReactElement {
-  const session = useAppSelector((state) => state.auth.session);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    getProfile();
-  }, [session]);
-
-  async function getProfile() {
-    try {
-      const user = supabase.auth.user();
-
-      let { data, error, status } = await supabase
-        .from<definitions['profiles']>('profiles')
-        .select(`*`)
-        .eq('id', user.id)
-        .single();
-
-      if (error && status !== 406) {
-        throw error;
-      }
-
-      if (data) {
-        dispatch(setProfile(data));
-      }
-    } catch (error:any) {
-      console.log(error.message);
-    }
-  }
-
-  useEffect(() => {
-    dispatch(setSession(supabase.auth.session()));
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      dispatch(setSession(session));
-    });
-  }, []);
   return (
-    <ApolloProvider client={apolloClient}>
-      <SWRConfig
-        value={{
-          // refreshInterval: 3000,
-          fetcher: (url) => axios.get(url).then((res) => res.data),
-        }}
-      >
-        <Component {...pageProps} />{' '}
-      </SWRConfig>
-    </ApolloProvider>
+    <SWRConfig
+      value={{
+        // refreshInterval: 3000,
+        fetcher: (url) => axios.get(url).then((res) => res.data),
+      }}
+    >
+      <Component {...pageProps} />{' '}
+    </SWRConfig>
   );
 }
 
